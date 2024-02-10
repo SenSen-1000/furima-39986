a5c9cb59f7bcf2a5aa530bbc707d9814471c8390
SenSen-1000 <chichisyun@gmail.com>
2024-02-10 18:01:01 +0900

    新規登録・ログイン・ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,         presence: true
+  validates :family_name,      presence: true, format: {with:/[^\x01-\x7Eｦ-ﾟ]+/}
+  validates :first_name,       presence: true, format: {with:/[^\x01-\x7Eｦ-ﾟ]+/}
+  validates :read_family_name, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :read_first_name,  presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :birth_date,       presence: true
+  validates :password, format: {
+    with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
+  },on: :create
 end
