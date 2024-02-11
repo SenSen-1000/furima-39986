a42c4e75df37585ec5e708edf08a2777010788aa
SenSen-1000 <chichisyun@gmail.com>
2024-02-11 19:15:23 +0900

    ActiveHash関連の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge_load
+  belongs_to :prefecture
+  belongs_to :number_of_day
+
+  validates :item_name, :item_explanation, :category, :condition, :delivery_charge_load, :prefecture, :number_of_day, :price, presence: true
+
+  validates :category_id,             numericality: { other_than: 1, message: "can't be blank" } 
+  validates :condition_id,            numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_charge_load_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id,           numericality: { other_than: 1, message: "can't be blank" } 
+  validates :number_of_day_id,        numericality: { other_than: 1, message: "can't be blank" } 
 end
