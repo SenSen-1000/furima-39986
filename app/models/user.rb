5e05ee34729a7fc897b2eecbd2a2cbcae11d226e
SenSen-1000 <chichisyun@gmail.com>
2024-02-11 17:23:56 +0900

    レビューの修正-2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :family_name,      presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}
-  validates :first_name,       presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :family_name,      presence: true, format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name,       presence: true, format: {with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :read_family_name, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
   validates :read_first_name,  presence: true, format: { with: /\A[ァ-ヶー]+\z/}
   validates :birth_date,       presence: true
