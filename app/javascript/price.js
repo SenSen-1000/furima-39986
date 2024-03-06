88a9a0cbcb761bf76774b429bc1d498b5a295a95
SenSen-1000 <chichisyun@gmail.com>
2024-03-06 20:07:24 +0900

    デプロイに伴うコード修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,28 +1,32 @@
-if (window.location.pathname === '/items/new') {
-  window.addEventListener('turbo:load', () => {
-    const priceInput = document.getElementById("item-price");
-    priceInput.addEventListener("input", () => {
-      const inputValue = priceInput.value;
-      const addTaxDom = document.getElementById("add-tax-price");
-      const profitDom = document.getElementById("profit");
+const price = () => {
+  if (window.location.pathname === '/items/new') {
+    window.addEventListener('turbo:load', () => {
+      const priceInput = document.getElementById("item-price");
+      priceInput.addEventListener("input", () => {
+        const inputValue = priceInput.value;
+        const addTaxDom = document.getElementById("add-tax-price");
+        const profitDom = document.getElementById("profit");
 
-      if (inputValue.trim() !== '') {
-        function calculateCommission(amount) {
-          const commissionRate = 0.1;
-          const commission = Math.floor(amount * commissionRate);
-          return commission;
+        if (inputValue.trim() !== '') {
+          function calculateCommission(amount) {
+            const commissionRate = 0.1;
+            const commission = Math.floor(amount * commissionRate);
+            return commission;
+          }
+          const amount = parseFloat(inputValue);
+          const commission = calculateCommission(amount);
+          const formattedCommission = commission.toLocaleString();
+          addTaxDom.innerHTML = formattedCommission;
+          const profit = amount - commission;
+          const formattedProfit = profit.toLocaleString();
+          profitDom.innerHTML = formattedProfit;
+        } else {
+          addTaxDom.innerHTML = "";
+          profitDom.innerHTML = "";
         }
-        const amount = parseFloat(inputValue);
-        const commission = calculateCommission(amount);
-        const formattedCommission = commission.toLocaleString();
-        addTaxDom.innerHTML = formattedCommission;
-        const profit = amount - commission;
-        const formattedProfit = profit.toLocaleString();
-        profitDom.innerHTML = formattedProfit;
-      } else {
-        addTaxDom.innerHTML = "";
-        profitDom.innerHTML = "";
-      }
+      });
     });
-  });
-}
+  }
+};
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
