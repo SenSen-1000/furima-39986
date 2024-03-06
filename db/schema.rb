1a595ecf3e3d42d06d8989751a1cf9692f010a54
SenSen-1000 <chichisyun@gmail.com>
2024-03-06 18:04:03 +0900

    マイグレーションファイル名の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_02_090914) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_29_133036) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
